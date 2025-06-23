a21abda8e26c7fd32f5d9272a9d2416171213d27
rohit1620 <malavrohit1620@gmail.com>
2025-06-23 11:11:33 +0530

    useState use

--- appagain/src/app/components/EventFunctionState.jsx
+++ appagain/src/app/components/EventFunctionState.jsx
@@ -1,14 +1,42 @@
 "use client";
+import { useState } from "react";
 
 const EventFunctionState = () => {
+  let value = "rohit";
+  const [val, setVal] = useState(value);
+  const sayHello = (name) => {
+    alert(`My name is ${name}`);
+  };
+
+  const changevalue = () => {
+    value = "malav";
+    setVal(value);
+  };
+
   return (
     <div>
       <h2>You know my name is EventFunctionState </h2>
-      <button
+      {/* <button
         className="bg-black text-white border-2 border-blue-800 px-4"
         onClick={() => alert("Rohit kumar malav")}
       >
         alert
+      </button> */}
+
+      <button
+        className="bg-black text-white border-2 border-blue-800 px-4"
+        onClick={() => sayHello("anjali")}
+      >
+        sayHello
+      </button>
+      <p className="text-9xl font-bold">
+        changeValue:<span className="text-yellow-600">{val}</span>
+      </p>
+      <button
+        className="bg-black text-white border-2 border-blue-800 px-4"
+        onClick={changevalue}
+      >
+        Change onClick
       </button>
     </div>
   );
