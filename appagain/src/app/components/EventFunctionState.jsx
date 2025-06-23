cb9e4609747682b44f001cd0a4c3a40e8ebd3a75
rohit1620 <malavrohit1620@gmail.com>
2025-06-23 10:46:00 +0530

    eventFunctionState code add

---
+++ appagain/src/app/components/EventFunctionState.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+const EventFunctionState = () => {
+  return (
+    <div>
+      <h2>You know my name is EventFunctionState </h2>
+      <button
+        className="bg-black text-white border-2 border-blue-800 px-4"
+        onClick={() => alert("Rohit kumar malav")}
+      >
+        alert
+      </button>
+    </div>
+  );
+};
+
+export default EventFunctionState;
